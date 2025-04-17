41292e4591c7504e052f59d9a8ba7791b31f91b3
banban525 <banban525@users.noreply.github.com>
2025-04-17 17:48:08 +0900

    Redesigned add-on settings

--- docker-entrypoint_addon.sh
+++ docker-entrypoint_addon.sh
@@ -1,56 +1,51 @@
 #!/usr/bin/env bashio
 
-if (bashio::config.has_value 'MQTT_BROKER'); then
-  export MQTT_BROKER=$(bashio::config "MQTT_BROKER")
+if (bashio::config.has_value 'mqtt.broker'); then
+  export MQTT_BROKER=$(bashio::config "mqtt.broker")
 fi
 if (bashio::config.has_value 'MQTT_OPTION_FILE'); then
-  export MQTT_OPTION_FILE=$(bashio::config "MQTT_OPTION_FILE")
+  export MQTT_OPTION_FILE=/addon_config/$(bashio::config "MQTT_OPTION_FILE")
 fi
-if (bashio::config.has_value 'MQTT_CA_FILE'); then
-  export MQTT_CA_FILE=$(bashio::config "MQTT_CA_FILE")
+if (bashio::config.has_value 'mqtt.ca_file'); then
+  export MQTT_CA_FILE=/ssl/$(bashio::config "mqtt.ca_file")
 fi
-if (bashio::config.has_value 'MQTT_CERT_FILE'); then
-  export MQTT_CERT_FILE=$(bashio::config "MQTT_CERT_FILE")
+if (bashio::config.has_value 'mqtt.cert_file'); then
+  export MQTT_CERT_FILE=/ssl/$(bashio::config "mqtt.cert_file")
 fi
-if (bashio::config.has_value 'MQTT_KEY_FILE'); then
-  export MQTT_KEY_FILE=$(bashio::config "MQTT_KEY_FILE")
+if (bashio::config.has_value 'mqtt.key_file'); then
+  export MQTT_KEY_FILE=/ssl/$(bashio::config "mqtt.key_file")
 fi
-if (bashio::config.has_value 'MQTT_BASE_TOPIC'); then
-  export MQTT_BASE_TOPIC=$(bashio::config "MQTT_BASE_TOPIC")
+if (bashio::config.has_value 'mqtt.base_topic'); then
+  export MQTT_BASE_TOPIC=$(bashio::config "mqtt.base_topic")
 fi
-if (bashio::config.has_value 'REST_API_HOST'); then
-  export REST_API_HOST=$(bashio::config "REST_API_HOST")
+if (bashio::config.has_value 'echonet.target_network'); then
+  export ECHONET_TARGET_NETWORK=$(bashio::config "echonet.target_network")
 fi
-if (bashio::config.has_value 'REST_API_PORT'); then
-  export REST_API_PORT=$(bashio::config "REST_API_PORT")
+if (bashio::config.has_value 'echonet.device_ip_list'); then
+  export ECHONET_DEVICE_IP_LIST=$(bashio::config "echonet.device_ip_list")
 fi
-if (bashio::config.has_value 'ECHONET_TARGET_NETWORK'); then
-  export ECHONET_TARGET_NETWORK=$(bashio::config "ECHONET_TARGET_NETWORK")
+if (bashio::config.has_value 'echonet.command_timeout'); then
+  export ECHONET_COMMAND_TIMEOUT=$(bashio::config "echonet.command_timeout")
 fi
-if (bashio::config.has_value 'ECHONET_DEVICE_IP_LIST'); then
-  export ECHONET_DEVICE_IP_LIST=$(bashio::config "ECHONET_DEVICE_IP_LIST")
+if (bashio::config.has_value 'echonet.disable_auto_device_discovery'); then
+  export ECHONET_DISABLE_AUTO_DEVICE_DISCOVERY=$(bashio::config "echonet.disable_auto_device_discovery")
 fi
-if (bashio::config.has_value 'ECHONET_COMMAND_TIMEOUT'); then
-  export ECHONET_COMMAND_TIMEOUT=$(bashio::config "ECHONET_COMMAND_TIMEOUT")
+if (bashio::config.has_value 'echonet.alias_file'); then
+  export ECHONET_ALIAS_FILE=/addon_config/$(bashio::config "echonet.alias_file")
 fi
-if (bashio::config.has_value 'ECHONET_DISABLE_AUTO_DEVICE_DISCOVERY'); then
-  export ECHONET_DISABLE_AUTO_DEVICE_DISCOVERY=$(bashio::config "ECHONET_DISABLE_AUTO_DEVICE_DISCOVERY")
+if (bashio::config.has_value 'echonet.legacy_multi_nic_mode'); then
+  export ECHONET_LEGACY_MULTI_NIC_MODE=$(bashio::config "echonet.legacy_multi_nic_mode")
 fi
-if (bashio::config.has_value 'ECHONET_ALIAS_FILE'); then
-  export ECHONET_ALIAS_FILE=$(bashio::config "ECHONET_ALIAS_FILE")
-fi
-if (bashio::config.has_value 'ECHONET_LEGACY_MULTI_NIC_MODE'); then
-  export ECHONET_LEGACY_MULTI_NIC_MODE=$(bashio::config "ECHONET_LEGACY_MULTI_NIC_MODE")
-fi
-if (bashio::config.has_value 'ECHONET_UNKNOWN_AS_ERROR'); then
-  export ECHONET_UNKNOWN_AS_ERROR=$(bashio::config "ECHONET_UNKNOWN_AS_ERROR")
+if (bashio::config.has_value 'echonet.unknown_as_error'); then
+  export ECHONET_UNKNOWN_AS_ERROR=$(bashio::config "echonet.unknown_as_error")
 fi
 
 
 
+INGRESS_PORT=$(bashio::addon.ingress_port)
 INGRESS_URL=$(bashio::addon.ingress_url)
 INGRESS_ENTRY=$(bashio::addon.ingress_entry)
+export REST_API_PORT=INGRESS_PORT
 export REST_API_ROOT=$INGRESS_ENTRY
-bashio::log.info "REST_API_ROOT=$REST_API_ROOT"
 
 npm run start:built
